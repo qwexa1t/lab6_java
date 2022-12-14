f8ace47696b6a6d92b7cfe7ca485d24758b623e6
wasdv <wasdv@DESKTOP-MNN8FME>
2022-12-15 01:11:52 +0700

    Добавил исключения

--- src/Square.java
+++ src/Square.java
@@ -21,6 +21,16 @@ public class Square extends Figure
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите сторону:");
         side = scanner.nextInt();
+        try
+        {
+            if (side < 0) throw new IllegalArgumentException("Negative number.");
+            if (side > 100) throw new IllegalArgumentException("Large number. Maximum value 100");
+        }
+        catch (IllegalArgumentException e)
+        {
+            e.printStackTrace(); //Вывод номера строки, в которой произошло исключение
+            System.exit(1);
+        }
         Count++;
     }
     public float area() { return side * side; }

b5dda24617f1b6c135ec6364d3bafc7684b39c7f
wasdv <wasdv@DESKTOP-MNN8FME>
2022-12-15 19:53:25 +0700

    Добавил проверку на ввод количества объектов и обновил работу со статик переменными

--- src/Main.java
+++ src/Main.java
@@ -35,11 +35,16 @@ public class Main
                         System.out.println("\nВы выбрали квадрат\n");
                         System.out.println("Пример");
                         Square square = new Square();
+                        Square square1 = new Square();
+                        Square square2 = new Square();
                         square.print();
                         System.out.println("\nВведите данные: ");
-                        square.set();
-                        square.print();
-                        System.out.println("Количество созданных квадратов: " + square.GetCount());
+                        square1.set();
+                        square1.print();
+                        System.out.println("\nВведите данные: ");
+                        square2.set();
+                        square2.print();
+                        Square.GetCount(); //Вывод количества объектов
                         firstin = false;
                     }
                     else
@@ -53,7 +58,11 @@ public class Main
                     Rectangle rectangle = new Rectangle(7);
                     rectangle.print();
                     System.out.println("\nСколько Прямоугольников создать? (1-5)");
-                    kolvo = scanner.nextInt();
+                    while (( kolvo = scanner.nextInt()) <= 0 || kolvo > 5)
+                    {
+                        System.out.print("\nВы ввели число >5 или <1. Повторите попытку.\n");
+                        System.out.print("\nСколько Прямоугольников создать? (1-5)");
+                    }
                     Rectangle[] rectangle_arr = new Rectangle[5];
                     for (int i = 0; i < kolvo; i++)
                     {
@@ -86,8 +95,12 @@ public class Main
                     System.out.println("Пример");
                     Circle circle = new Circle(6);
                     circle.print();
-                    System.out.println("\nСколько Прямоугольников создать? (1-5)");
-                    kolvo = scanner.nextInt();
+                    System.out.println("\nСколько Кругов создать? (1-5)");
+                    while (( kolvo = scanner.nextInt()) <= 0 || kolvo > 5)
+                    {
+                        System.out.print("\nВы ввели число >5 или <1. Повторите попытку.\n");
+                        System.out.print("\nСколько Кругов создать? (1-5)");
+                    }
                     //Объявление двумерного динамического массива объектов:
                     Circle[][] circle_arr = new Circle[5][5];
                     for (int i = 0; i < kolvo; i++)
